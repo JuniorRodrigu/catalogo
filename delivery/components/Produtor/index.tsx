0ffb53c88a8f8b1ff1309bfdf08b7eded41c52f6
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-06-02 13:48:37 -0300

    comeco

---
+++ delivery/components/Produtor/index.tsx
@@ -0,0 +1,27 @@
+import style from './styles.module.css';
+
+export default function Produtor  () {
+    return (
+        <div className={style.container}>
+            <div className={style.head}></div>
+
+            <div className={style.info}>
+                <div className={style.img}>
+                <img src="/tmp/aaaa.png" alt=""  />
+                </div>
+               
+            <div className={style.catNome}>transparente </div>
+<div className={style.nome}>Animal</div>
+<div className={style.price}>R$ 10</div></div>
+
+
+
+
+
+
+        </div>
+
+    );
+
+
+}
---
+++ delivery/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div >
+      23432
+    </div>
+  )
+}
